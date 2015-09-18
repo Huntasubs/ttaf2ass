724b6e4846ee98b51ef32f7e148099c772504dfa
Sova <huntasubs@gmail.com>
2015-09-18 01:32:25 -0400

    just re-reading

--- ttaf2ass.sh
+++ ttaf2ass.sh
@@ -1,4 +1,4 @@
- #!/bin/bash
+#!/bin/bash
 #Ёбаный кот, вот это говноскрипт, Так что мы тут делаем...
 
 #Вариаблы, они няши
@@ -11,8 +11,8 @@ header3="Style: Default,Open Sans Semibold,36,\&H00FFFFFF,\&H000000FF,\&H0002071
 header4="\n\n[Events]\nFormat: Layer, Start, End, Style, Name, MarginL, MarginR, MarginV, Effect, Text\n"
 maxheader=$(echo $header$header2$header3$header4)
 perl -wne "print if /<div xml:lang=\"$lang\"/ .. /<\/div>/" $script  | \
-sed 's/<br \/>/\\N /g' | sed 's/<\/p>/ /g' | sed 's/<p\ begin\=\"0/Dialogue: 0,/g' | \
-sed 's/\"\ end\=\"0/,/g' | sed 's/" style="1">/,Main,,0000,0000,0000,,/g' | \
-tr -d "\t\n\r\f" | sed 's/Dialogue/\nDialogue/g' |sed 's/<\/div>//g' | \
-sed "s/    <div xml:lang=\"$lang\">/$maxheader/g"  > $lang.ass
+    sed 's/<br \/>/\\N /g' | sed 's/<\/p>/ /g' | sed 's/<p\ begin\=\"0/Dialogue: 0,/g' | \
+    sed 's/\"\ end\=\"0/,/g' | sed 's/" style="1">/,Main,,0000,0000,0000,,/g' | \
+    tr -d "\t\n\r\f" | sed 's/Dialogue/\nDialogue/g' |sed 's/<\/div>//g' | \
+    sed "s/    <div xml:lang=\"$lang\">/$maxheader/g"  > $lang.ass
 #sed -n -e '/\\N.*\\N/p' $lang.ass | sed 's/0000,,/0000,,{\\an8}/g' | sed '0,/RE/s/\\N/\N{\\an2}/' >> $lang.ass
